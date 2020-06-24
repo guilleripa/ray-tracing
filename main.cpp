db7f2fb8f7810c7904551e59d7f227dbfb6e495e
guilleripa <guilleripa21@gmail.com>
2020-06-23 22:01:19 -0300

    Include elapsed time in logs.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include "../tinyxml2.h"
 #include "OpenGL-basico/Whitted.h"
-
+#include <iostream>
+#include <chrono>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
 	try {
+		auto start = chrono::system_clock::now();
 		Scene scene = Scene().loadScene();
 		Whitted().run(scene);
+		auto end = chrono::system_clock::now();
+		chrono::duration<double> elapsed_seconds = end - start;
+		cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 	}
 	catch (int param) {
 		if (param == 6) {
